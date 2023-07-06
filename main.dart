1ae6da7cfb92cd5bb1077af837365e9fd5174827
Khoirun Nisa <134363442+nisakhoirun@users.noreply.github.com>
2023-07-06 13:06:11 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:statless_dan_statefull/appStatefull.dart';
+// import 'package:statefull_dan_stateless/appStateless.dart';
+
+void main(List<String> args) {
+  runApp(const MyKel());
+}
